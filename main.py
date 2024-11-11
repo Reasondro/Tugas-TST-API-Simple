061ad96972e7e4685a7e5569fe66d5b6b8b01752
Reasondro <alessandrohasian16@gmail.com>
2024-11-12 00:22:58 +0700

    is it this easy?

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "Nama" : "Alessandro JH",
+        "NIM" : "18222025",
+        "Bisnis" : "ToFood",
+        "Recipe" : "Soto Barbeque",
+        "Recipe Screening": "Passed",
+        "Recipe Recomendation": "Add more spicess"
+            }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
